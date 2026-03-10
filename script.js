9f340e329ecf8dd7ec916d914a864a174b0af276
Isaadr <i.mosquerad@uniandes.edu.co>
2026-03-10 17:28:47 +0000

    5

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const texto = document.getElementById("texto");
 
+const texto = ["Sagitario: eres una chica condenada al éxito", "Virgo: cosas grandes te esperan sigue así", "Tauro: la felicidad está dentro de ti"];
+let textoOriginal = true;
+let iTxt = 0;
+
+if(undefined === true) {
+
+}else {
+    texto.innerText = "Sagitario"
+}
+
+console.log(texto);
+texto.addEventListener("click", () => {
+    texto.innerText = textos [iTxt];
+    iTxt += 1; 
+    if(iTxt >= textos.length){
+      iTxt = 0  
+    }
+
+});
